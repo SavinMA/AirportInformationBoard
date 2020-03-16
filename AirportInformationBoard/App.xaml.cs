805f58474e89409828ba03e140b44c54ce600971
Савин Максим <ctok81@gmail.com>
2020-03-16 09:34:46 +0500

    bugs fix

--- AirportInformationBoard/App.xaml.cs
+++ AirportInformationBoard/App.xaml.cs
@@ -17,39 +17,16 @@ namespace AirportInformationBoard
     /// </summary>
     public partial class App : Application
     {
-        private CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
         protected override void OnStartup(StartupEventArgs e)
         {
-            LoadSchedule();
-
             App.Current.DispatcherUnhandledException += DispatcherUnhandledException;
 
             base.OnStartup(e);
         }
 
-        protected override void OnExit(ExitEventArgs e)
-        {
-            cancellationTokenSource.Cancel();
-
-            base.OnExit(e);
-        }
-
         private void DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
         {
             MessageBox.Show($"{e.Exception} \r\n {e.Exception.InnerException}");
-        }
-
-        private async void LoadSchedule()
-        {
-            try
-            {
-                var text = await File.ReadAllTextAsync(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Schedules"), cancellationTokenSource.Token);
-                var shedules = Serializator<List<Schedule>>.DeSerializeFromString(text);
-            }
-            catch(Exception e)
-            {
-                MessageBox.Show($"{e} \r\n {e.InnerException}");
-            }
         }
     }
 }
